ca243b7afe11a806c7dbb86ead7b01fb8871d98c
Mike Shultz <shultzm@gmail.com>
2019-02-19 02:01:14 -0700

    version 0.1.0b2

--- sink/__init__.py
+++ sink/__init__.py
@@ -1,5 +1,5 @@
 """ Sink - Arch Linux AUR Package Installer """
-__version__ = '0.1.0b1'
+__version__ = '0.1.0b2'
 __author__ = 'Mike Shultz'
 __copyright__ = "Copyright 2018 Mike Shultz"
 __license__ = 'GPLv3'
